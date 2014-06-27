fb3e3a4c29af45344a116b8dfc2fc883220750b4
AlloyDev <alloy.stas@gmail.com>
2014-06-27 12:32:02 +0400

    - add 100 arc4random_uniform-placed buttons - scale them via scrollViewDidZoom delegate method

--- UIScrollViewSubviewsScaleExample/SVTViewController.m
+++ UIScrollViewSubviewsScaleExample/SVTViewController.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) NSArray *buttonsArray;
 
 @end
 
@@ -23,6 +24,29 @@
 	// Do any additional setup after loading the view, typically from a nib.
   self.scrollView.minimumZoomScale = 1;
   self.scrollView.maximumZoomScale = 20.0;
+  
+  [self fillButtonsArray];
+}
+
+- (void)fillButtonsArray
+{
+  NSMutableArray *mutButtonsArray = [[NSMutableArray alloc] init];
+  for (int i = 0; i < 100; i++) {
+    NSInteger x = arc4random_uniform(CGRectGetWidth(self.view.frame));
+    NSInteger y = arc4random_uniform(CGRectGetHeight(self.view.frame));
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(x, y, 25.0, 25.0)];
+    [button setImage:[UIImage imageNamed:@"icon_button_productOverlay"] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(plusButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    [mutButtonsArray addObject:button];
+    [self.contentView addSubview:button];
+  }
+  self.buttonsArray = mutButtonsArray;
+}
+
+- (void)plusButtonPressed
+{
+  NSLog(@"button pressed");
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,9 +60,18 @@
   return self.contentView;
 }
 
-- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
 {
+  NSLog(@"scale: %f", _scrollView.zoomScale);
+  [self scaleButtons];
   
+}
+
+- (void)scaleButtons
+{
+  for (UIButton *btn in self.buttonsArray) {
+    btn.transform = CGAffineTransformMakeScale(1 / _scrollView.zoomScale, 1 / _scrollView.zoomScale);
+  }
 }
 
 @end
